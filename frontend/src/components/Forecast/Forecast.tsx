b2b34626f1273801ff63212b511b4e6725450c4b
Daniel <freelinex7@gmail.com>
2026-09-11 00:07:30 +0300

    Implemented chart period selection

--- frontend/src/components/Forecast/Forecast.tsx
+++ frontend/src/components/Forecast/Forecast.tsx
@@ -19,27 +19,56 @@ import {
   getForecastDate,
   mapForecastToChartData,
 } from "../../services/forecastToChartData";
+import { ChartPeriods } from "../../types/enums";
+import classNames from "classnames";
+import { Bars } from "react-loader-spinner";
 
 export const Forecast = () => {
   const [forecastData, setForecastData] = useState<ForecastData[] | null>(null);
   const [selectedPoint, setSelectedPoint] = useState<ChartPoint>();
+
   const [forecastInterval, setForecastInterval] = useState("60");
+  const [period, setPeriod] = useState(ChartPeriods.day);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getForecast().then((response) => {
-      if (response) {
-        setForecastData(response);
-      }
-    });
+    getForecast()
+      .then((response) => {
+        if (response) {
+          setForecastData(response);
+        }
+      })
+      .finally(() => {
+        setTimeout(() => setIsLoading(false), 2000);
+      });
   }, []);
 
   const data = mapForecastToChartData(forecastData ?? []);
 
   const today = getForecastDate(new Date());
 
+  const [day, month, year] = today.split("/").map(Number);
+
+  const start = new Date(Date.UTC(year, month - 1, day));
+  const end = new Date(start);
+
+  const daysByPeriod: Record<ChartPeriods, number> = {
+    [ChartPeriods.day]: 1,
+    [ChartPeriods.week]: 7,
+    [ChartPeriods.month]: 30,
+  };
+
+  end.setUTCDate(end.getUTCDate() + daysByPeriod[period]);
+
+  const startMs = start.getTime();
+  const endMs = end.getTime();
+
   const visibleData = data
     .filter((element) => {
-      return element.date === today;
+      const timestamp = new Date(element.timestamp).getTime();
+
+      return timestamp >= startMs && timestamp < endMs;
     })
     .filter((_element, index) => {
       if (forecastInterval === "60") {
@@ -48,6 +77,10 @@ export const Forecast = () => {
 
       if (forecastInterval === "30") {
         return index % 2 === 0;
+      }
+
+      if (forecastInterval === "day") {
+        return index % 96 === 0;
       }
 
       return true;
@@ -75,6 +108,11 @@ export const Forecast = () => {
     }
   };
 
+  const handlePeriodChange = (period: ChartPeriods) => {
+    setForecastInterval("60");
+    setPeriod(period);
+  };
+
   return (
     <section className="price-forecast">
       <header className="price-forecast__header">
@@ -82,171 +120,211 @@ export const Forecast = () => {
           <h2 className="price-forecast__title">Price Forecast</h2>
         </div>
 
-        <div className="price-forecast__controls">
-          <div className="price-forecast__periods">
-            <button
-              type="button"
-              className="price-forecast__period-button price-forecast__period-button--active"
+        {!isLoading && (
+          <div className="price-forecast__controls">
+            <div className="price-forecast__periods">
+              <button
+                type="button"
+                onClick={() => handlePeriodChange(ChartPeriods.day)}
+                className={classNames("price-forecast__period-button", {
+                  "price-forecast__period-button--active":
+                    period === ChartPeriods.day,
+                })}
+              >
+                24h
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handlePeriodChange(ChartPeriods.week)}
+                className={classNames("price-forecast__period-button", {
+                  "price-forecast__period-button--active":
+                    period === ChartPeriods.week,
+                })}
+              >
+                1w
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handlePeriodChange(ChartPeriods.month)}
+                className={classNames("price-forecast__period-button", {
+                  "price-forecast__period-button--active":
+                    period === ChartPeriods.month,
+                })}
+              >
+                1m
+              </button>
+            </div>
+
+            <select
+              className="price-forecast__interval"
+              value={forecastInterval}
+              onChange={(e) => setForecastInterval(e.target.value)}
             >
-              24h
-            </button>
-
-            <button type="button" className="price-forecast__period-button">
-              1w
-            </button>
-
-            <button type="button" className="price-forecast__period-button">
-              1m
-            </button>
+              {period !== ChartPeriods.week &&
+                period !== ChartPeriods.month && (
+                  <option value="15">15 min</option>
+                )}
+              {period !== ChartPeriods.month && (
+                <option value="30">30 min</option>
+              )}
+              <option value="60">hourly</option>
+              {period === ChartPeriods.month && (
+                <option value="day">daily</option>
+              )}
+            </select>
           </div>
-
-          <select
-            className="price-forecast__interval"
-            defaultValue="60"
-            onChange={(e) => setForecastInterval(e.target.value)}
-          >
-            <option value="15">15 min</option>
-            <option value="30">30 min</option>
-            <option value="60">hourly</option>
-          </select>
-        </div>
+        )}
       </header>
 
-      <div className="price-forecast__unit">€/MWh</div>
+      {isLoading ? (
+        <div className="price-forecast__loader">
+          <Bars color="#0047F4" />
+        </div>
+      ) : (
+        <>
+          <div className="price-forecast__unit">€/MWh</div>
 
-      <div className="price-forecast__chart">
-        <ResponsiveContainer width="100%" height="100%">
-          <ComposedChart
-            data={visibleData}
-            accessibilityLayer={false}
-            onMouseMove={handleChartMouseMove}
-            margin={{
-              top: 20,
-              right: 18,
-              left: -12,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid stroke="#ececf2" vertical={false} />
+          <div className="price-forecast__chart">
+            <ResponsiveContainer width="100%" height="100%">
+              <ComposedChart
+                data={visibleData}
+                accessibilityLayer={false}
+                onMouseMove={handleChartMouseMove}
+                margin={{
+                  top: 20,
+                  right: 18,
+                  left: -12,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid stroke="#ececf2" vertical={false} />
 
-            <XAxis
-              dataKey="slot"
-              tickFormatter={(slot: number) => data[slot]?.time.trim() ?? ""}
-              axisLine={false}
-              tickLine={false}
-              interval={4}
-              tick={{
-                fill: "#808080",
-                fontSize: 12,
-              }}
-              dy={10}
-            />
+                <XAxis
+                  dataKey="slot"
+                  tickFormatter={(slot: number) =>
+                    data[slot]?.time.trim() ?? ""
+                  }
+                  axisLine={false}
+                  tickLine={false}
+                  interval={4}
+                  tick={{
+                    fill: "#808080",
+                    fontSize: 12,
+                  }}
+                  dy={10}
+                />
 
-            <YAxis
-              domain={[0, 200]}
-              ticks={[0, 40, 80, 120, 160, 200]}
-              axisLine={false}
-              tickLine={false}
-              tick={{
-                fill: "#808080",
-                fontSize: 12,
-              }}
-            />
+                <YAxis
+                  domain={[0, 200]}
+                  ticks={[0, 40, 80, 120, 160, 200]}
+                  axisLine={false}
+                  tickLine={false}
+                  tick={{
+                    fill: "#808080",
+                    fontSize: 12,
+                  }}
+                />
 
-            <Tooltip content={<CustomTooltip />} cursor={false} />
+                <Tooltip content={<CustomTooltip />} cursor={false} />
 
-            <Area
-              type="linear"
-              dataKey="rangeBase"
-              stackId="range"
-              stroke="none"
-              fill="transparent"
-              activeDot={false}
-              legendType="none"
-              isAnimationActive={false}
-            />
+                <Area
+                  type="linear"
+                  dataKey="rangeBase"
+                  stackId="range"
+                  stroke="none"
+                  fill="transparent"
+                  activeDot={false}
+                  legendType="none"
+                  isAnimationActive={false}
+                />
 
-            <Area
-              type="linear"
-              dataKey="rangeDiff"
-              stackId="range"
-              stroke="none"
-              activeDot={false}
-              fill="#f0f0f8"
-              fillOpacity={0.9}
-              name="Prices range"
-              isAnimationActive={false}
-            />
+                <Area
+                  type="linear"
+                  dataKey="rangeDiff"
+                  stackId="range"
+                  stroke="none"
+                  activeDot={false}
+                  fill="#f0f0f8"
+                  fillOpacity={0.9}
+                  name="Prices range"
+                  isAnimationActive={false}
+                />
 
-            <ReferenceLine
-              y={selectedPoint?.actual ?? selectedPoint?.forecast}
-              stroke="#9d9da7"
-              strokeDasharray="6 6"
-            />
+                <ReferenceLine
+                  y={selectedPoint?.actual ?? selectedPoint?.forecast}
+                  stroke="#9d9da7"
+                  strokeDasharray="6 6"
+                />
 
-            <ReferenceLine
-              x={selectedPoint?.slot}
-              stroke="#b8b8c0"
-              strokeDasharray="6 6"
-            />
+                <ReferenceLine
+                  x={selectedPoint?.slot}
+                  stroke="#b8b8c0"
+                  strokeDasharray="6 6"
+                />
 
-            <Line
-              type="linear"
-              dataKey="actual"
-              stroke="#6E55FF"
-              strokeWidth={1.2}
-              dot={false}
-              activeDot={{
-                r: 4,
-                fill: "#EAF5FF",
-                stroke: "#007DFF",
-                strokeWidth: 1,
-              }}
-              name="Actual price"
-              connectNulls={true}
-              isAnimationActive={false}
-            />
+                <Line
+                  type="linear"
+                  dataKey="actual"
+                  stroke="#6E55FF"
+                  strokeWidth={1.2}
+                  dot={false}
+                  activeDot={{
+                    r: 4,
+                    fill: "#EAF5FF",
+                    stroke: "#007DFF",
+                    strokeWidth: 1,
+                  }}
+                  name="Actual price"
+                  connectNulls={true}
+                  isAnimationActive={false}
+                />
 
-            <Line
-              type="linear"
-              dataKey="forecast"
-              stroke="#007DFF"
-              strokeWidth={1.2}
-              strokeDasharray="3 3"
-              dot={false}
-              activeDot={{
-                r: 4,
-                fill: "#EAF5FF",
-                stroke: "#007DFF",
-                strokeWidth: 1,
-              }}
-              name="Forecast"
-              connectNulls={true}
-              isAnimationActive={false}
-            />
+                <Line
+                  type="linear"
+                  dataKey="forecast"
+                  stroke="#007DFF"
+                  strokeWidth={1.2}
+                  strokeDasharray="3 3"
+                  dot={false}
+                  activeDot={{
+                    r: 4,
+                    fill: "#EAF5FF",
+                    stroke: "#007DFF",
+                    strokeWidth: 1,
+                  }}
+                  name="Forecast"
+                  connectNulls={true}
+                  isAnimationActive={false}
+                />
 
-            <Legend
-              verticalAlign="bottom"
-              height={44}
-              iconType="plainline"
-              wrapperStyle={{
-                fontSize: "12px",
-                color: "#777780",
-                paddingTop: "16px",
-              }}
-              labelStyle={{
-                color: "#535353",
-              }}
-            />
-          </ComposedChart>
-        </ResponsiveContainer>
-      </div>
+                <Legend
+                  verticalAlign="bottom"
+                  height={44}
+                  iconType="plainline"
+                  wrapperStyle={{
+                    fontSize: "12px",
+                    color: "#777780",
+                    paddingTop: "16px",
+                  }}
+                  labelStyle={{
+                    color: "#535353",
+                  }}
+                />
+              </ComposedChart>
+            </ResponsiveContainer>
+          </div>
 
-      <footer className="price-forecast__footer">
-        <span>Forecast generated today, {shortTime.toLocaleUpperCase()}</span>
+          <footer className="price-forecast__footer">
+            <span>
+              Forecast generated today, {shortTime.toLocaleUpperCase()}
+            </span>
 
-        <span>Data sources: ENTSO-E</span>
-      </footer>
+            <span>Data sources: ENTSO-E</span>
+          </footer>
+        </>
+      )}
     </section>
   );
 };
