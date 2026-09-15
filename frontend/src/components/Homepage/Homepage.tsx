4ba95cad52d1356d0aae8ffa60eebb2eb1d9359d
Daniel <freelinex7@gmail.com>
2026-09-15 19:45:09 +0300

    Add hero section

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -1,11 +1,20 @@
 import "./Homepage.scss";
+import { HeroSection } from "./HeroSection";
+import { InfoSection } from "./InfoSection";
 
 export const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="homepage__titleBox">
-        <h1 className="homepage__titleBox__title">Landing Page</h1>
-      </div>
+      <HeroSection />
+
+      <main className="homepage__main">
+        <section className="homepage__main__logo-box">
+          <h4 className="homepage__main__logo-box__title">We are</h4>
+          <div className="homepage__main__logo-box__logo-container"></div>
+        </section>
+
+        <InfoSection />
+      </main>
     </div>
   );
 };
