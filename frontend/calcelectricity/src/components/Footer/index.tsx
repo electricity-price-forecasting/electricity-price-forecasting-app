64bcf72d2c85a9ac5bf6c643bb0f36096750bf47
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-09-25 08:42:56 +0300

    make upd (added links (YouTube,LinkedIn)

--- frontend/calcelectricity/src/components/Footer/index.tsx
+++ frontend/calcelectricity/src/components/Footer/index.tsx
@@ -139,8 +139,8 @@ export default function Footer() {
         <span>© 2026 Voltio. All rights reserved.</span>
         <div className="footer__social">
           <a href="https://github.com/electricity-price-forecasting/electricity-price-forecasting-app.git" aria-label="GitHub"><IconGithub /></a>
-          <a href="#" aria-label="YouTube"><IconYoutube /></a>
-          <a href="#" aria-label="LinkedIn"><IconLinkedin /></a>
+          <a href="https://www.youtube.com/watch?v=CF5I-6jdejQ&t=3s" aria-label="YouTube"><IconYoutube /></a>
+          <a href="https://www.linkedin.com/in/maksym-tsyrulnyk-994892201/overlay/1789919249703/single-media-viewer/?profileId=ACoAADOQ3AABucged1pSwthvlEBm55RGJ7maQF0" aria-label="LinkedIn"><IconLinkedin /></a>
         </div>
       </div>
     </footer>
